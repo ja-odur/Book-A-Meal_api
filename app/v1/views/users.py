db1a64c857a278eb08f4da494234bdddf01fc379
Odur Joseph <odurjoseph8@gmail.com>
2018-05-03 00:57:13 +0300

    ft-token-auth added token auth to login

--- app/v1/views/users.py
+++ app/v1/views/users.py
@@ -1,10 +1,11 @@
 from flasgger import swag_from
 from flask import jsonify, request, make_response, Blueprint
-from run import app
 import jwt
 import datetime
 
 from app.v1.models.models import DbUsers, DbCaterers
+
+SECRET_KEY ='secretKey4512yek'
 
 user_db = DbUsers()
 caterer_db = DbCaterers()
@@ -16,6 +17,13 @@ users = Blueprint('users', __name__, url_prefix='/api/v1')
 @swag_from('api_doc/user_registration.yml')
 def register_user():
     data = request.get_json()
+    try:
+        if data['category'] and data['email'] and data['username'] \
+                and data['password'] and data['confirm_password'] and data['address']:
+            pass
+    except:
+        return make_response(jsonify({'message': 'Invalid data format'}), 403)
+
     if data['password'] == data['confirm_password']:
         if data['category'] == 'user':
             add_user = user_db.add_user(data['email'], data['username'], data['password'], data['address'])
@@ -40,11 +48,19 @@ def register_user():
                 message.encode('utf-8')
                 return make_response(jsonify(dict(message=message)), 403)
 
+    return make_response(jsonify({'message': 'category can either be user or caterer.'}), 403)
+
 
 @users.route('/auth/login', methods=['POST'])
 @swag_from('api_doc/user_login.yml')
 def login():
     data = request.get_json()
+    try:
+        if data['category'] and data['username']:
+            pass
+    except KeyError:
+        return make_response(jsonify({'message': 'Invalid Username or Password'}), 401)
+
     if data['category'] == 'user':
         user_info = user_db.get_user(data['username'])
         if user_info:
@@ -56,9 +72,9 @@ def login():
                 token_string = str(category)+','+str(username)+','+str(email)
                 token = jwt.encode({'info': token_string,
                                     'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=30)},
-                                        app.config['SECRET_KEY'])
+                                        'SECRET_KEY')
                 return make_response(jsonify(dict(token=token.decode('UTF-8'))), 200)
-                # return make_response(jsonify({'message':'user successfully logged in'}), 200)
+
             else:
                 return make_response(jsonify({'message': 'Invalid Username or Password'}), 401)
         else:
@@ -70,8 +86,16 @@ def login():
         if caterer_info:
             user_password = caterer_info['password']
             if user_password == data['password']:
-                return make_response(jsonify({'message': 'user successfully logged in'}), 200)
+                category = 'caterer'
+                email = caterer_info['email']
+                username = caterer_info['username']
+                token_string = str(category) + ',' + str(username) + ',' + str(email)
+                token = jwt.encode({'info': token_string,
+                                    'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=30)},
+                                   'SECRET_KEY')
+                return make_response(jsonify(dict(token=token.decode('UTF-8'))), 200)
+
             else:
                 return make_response(jsonify({'message': 'Invalid Username or Password'}), 201)
-        else:
-            return make_response(jsonify({'message': 'Invalid Username or Password'}), 401)
+
+    return make_response(jsonify({'message': 'Invalid Username or Password'}), 401)
