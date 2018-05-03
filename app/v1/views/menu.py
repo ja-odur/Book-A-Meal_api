eae88fb983962303975a572cc01e16ea6e87e42b
Odur Joseph <odurjoseph8@gmail.com>
2018-05-03 10:10:13 +0300

    ft-token-auth added header containing tokens in menu test request and added a parameter to hold the token in the yaml files

--- app/v1/views/menu.py
+++ app/v1/views/menu.py
@@ -1,5 +1,6 @@
 from flasgger import swag_from
 from flask import jsonify, request, make_response, Blueprint
+from app.v1.views.decorators import token_required
 
 from app.v1.models.models import DbMenu
 
@@ -10,9 +11,11 @@ menu = Blueprint('menu', __name__, url_prefix='/api/v1')
 
 @menu.route('/menu/', methods=['POST'])
 @swag_from('api_doc/create_menu.yml')
-def create_menu():
+@token_required(admin=True)
+def create_menu(current_user):
+    caterer = current_user[1]
     data = request.get_json()
-    created_menu = menu_db.create_menu(caterer='default', daily_menu=data['menu'])
+    created_menu = menu_db.create_menu(caterer=caterer, daily_menu=data['menu'])
 
     if created_menu:
         message = 'Menu {} successfully added.'.format(data['menu'])
@@ -22,7 +25,8 @@ def create_menu():
 
 @menu.route('/menu/', methods=['GET'])
 @swag_from('api_doc/get_menu.yml')
-def get_menu():
+@token_required()
+def get_menu(current_user):
     menu = menu_db.get_menu()
     message = 'Todays menu {}.'.format(menu)
     return make_response(jsonify(message=message), 200)
