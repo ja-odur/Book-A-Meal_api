8ecaef3be027d7741dc771268a6fe8ff7936e95d
Odur Joseph <odurjoseph8@gmail.com>
2018-05-18 00:47:16 +0300

    ft-develop refactored caterer db

--- app/v1/models/caterers.py
+++ app/v1/models/caterers.py
@@ -13,9 +13,9 @@ class DbCaterers:
 
     def add_caterer(self, email, username, password, address, category='caterer', brand_name=''):
         if email not in self.all_emails:
-            user_exists = self.all_caterers.get(username, False)
+            caterer_exists = self.all_caterers.get(username, False)
 
-            if not user_exists:
+            if not caterer_exists:
                 brand_name_final = brand_name if brand_name else username
                 self.all_caterers[username] = dict(email=email, username=username, password=password, address=address,
                                                    category=category, brand_name=brand_name_final)
@@ -25,15 +25,15 @@ class DbCaterers:
         return False
 
     def get_caterer(self, username):
-        user = self.all_caterers.get(username, False)
-        return user
+        caterer = self.all_caterers.get(username, False)
+        return caterer
 
     def get_all_caterers(self):
         return self.all_caterers
 
     def remove_caterer(self, username):
-        user = self.all_caterers.get(username, False)
-        if user:
+        caterer = self.all_caterers.get(username, False)
+        if caterer:
             del self.all_caterers[username]
             return True
         return False
