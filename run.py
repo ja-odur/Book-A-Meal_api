1eb444699ff8943f2fb2d0cba2230c5a6bcf93b2
Odur Joseph <odurjoseph8@gmail.com>
2018-05-18 18:27:50 +0300

    ft-develop refactored run.py

--- run.py
+++ run.py
@@ -3,14 +3,11 @@ from app.v1.views.menu import menu
 from app.v1.views.users import users
 from flasgger import Swagger
 from flask import Flask
-#from os import environ
 
 from app.v1.views.orders import orders
 
 
 app = Flask(__name__, instance_relative_config=True)
-# app.config.from_envvar('APP_SETTINGS')
-# app.config['API_KEY'] = environ.get('SECRET_KEY')
 
 swagger = Swagger(app)
 
