78548e9775e06974fa073f0184a60e08b50bd852
Odur Joseph <odurjoseph8@gmail.com>
2018-05-27 14:18:19 +0300

    ft-Develop refatored DbOrder module to use only on data structure

--- app/v1/models/orders.py
+++ app/v1/models/orders.py
@@ -9,7 +9,6 @@ class DbOrders:
     """
     def __init__(self):
         self.orders_customers = dict()
-        self.orders_caterers = dict()
         self.orders_history = dict()
         self.order_expiry_time = datetime.timedelta(minutes=1).total_seconds()
         self.id_count = 1
@@ -17,15 +16,12 @@ class DbOrders:
     def add_order(self, customer, caterer, meal):
         order_time = datetime.datetime.now()
         order_customer_format = dict(order_id=self.id_count, meal=meal, caterer=caterer, cleared=False,
-                                     order_time=order_time)
-        order_caterer_format = dict(order_id=self.id_count, meal=meal, customer=customer, cleared=False
-                                    , order_time=order_time)
+                                     order_time=order_time, customer=customer)
+
         self.id_count += 1
         set_customer_order = False
-        set_caterer_order = False
 
         orders_customers = self.orders_customers.get(customer, False)
-        orders_caterers = self.orders_caterers.get(caterer, False)
 
         if not orders_customers:
             self.orders_customers[customer] = [order_customer_format]
@@ -34,27 +30,17 @@ class DbOrders:
             self.orders_customers[customer].append(order_customer_format)
             set_customer_order = True
 
-        if not orders_caterers:
-            self.orders_caterers[caterer] = [order_caterer_format]
-            set_caterer_order = True
-        else:
-            self.orders_caterers[caterer].append(order_caterer_format)
-            set_caterer_order = True
-
-        if set_customer_order and set_caterer_order:
+        if set_customer_order:
             return True
         return False
 
     def modify_order(self, customer, caterer, order_id, meal):
         time_now = datetime.datetime.now()
         orders = self.orders_customers.get(customer, False)
-        orders_caterers = self.orders_caterers.get(caterer, False)
 
-        if orders and orders_caterers:
+        if orders:
             match_found = False
-            matched_caterer_found = False
             counter_customer = 0
-            counter_caterer = 0
 
             while counter_customer < len(orders):
                 if orders[counter_customer]['order_id'] == order_id:
@@ -62,19 +48,11 @@ class DbOrders:
                     break
                 counter_customer += 1
 
-            while counter_caterer < len(orders_caterers):
-                if orders_caterers[counter_caterer]['order_id'] == order_id and \
-                        orders_caterers[counter_caterer]['customer'] == customer:
-                    matched_caterer_found = True
-                    break
-                counter_caterer += 1
-
-            if match_found and matched_caterer_found:
+            if match_found:
                 order_time = self.orders_customers[customer][counter_customer]['order_time']
                 elapse_time = time_now - order_time
                 if elapse_time.total_seconds() <= self.order_expiry_time:
                     self.orders_customers[customer][counter_customer]['meal'] = meal
-                    self.orders_caterers[caterer][counter_caterer]['meal'] = meal
 
                     return True
                 else:
@@ -82,9 +60,15 @@ class DbOrders:
 
         return False
 
-    def get_orders(self, caterer):
-        orders = self.orders_caterers.get(caterer, False)
-        return orders
+    def get_all_orders(self, caterer):
+        all_orders = []
+        for orders in self.orders_customers.values():
+            for order in orders:
+                if order['caterer'] == caterer:
+                    all_orders.append(order)
+        if all_orders:
+            return all_orders
+        return False
 
     def get_orders_per_user(self, user):
         orders = self.orders_customers.get(user, False)
@@ -97,29 +81,17 @@ class DbOrders:
             counter = 0
             for order in all_orders:
                 if order['order_id'] == order_id and not order['cleared']:
-                    caterer = order['caterer']
                     order_time = order['order_time']
                     elapse_time = time_now - order_time
                     if elapse_time.total_seconds() <= self.order_expiry_time:
-                        self.delete_caterer(caterer, order['order_id'])
                         del all_orders[counter]
                         return True
                 counter += 1
         return False
 
-    def delete_caterer(self, caterer, order_id):
-        all_orders = self.orders_caterers.get(caterer, False)
-        if all_orders:
-            counter = 0
-            for order in all_orders:
-                if order['order_id'] == order_id:
-                    del all_orders[counter]
-                    return True
-                counter += 1
-        return False
-
-    def clear_order(self, caterer, customer, order_id):
-        pass
+    def clear_order(self, caterer, order_id):
+        for customer in self.orders_customers.values():
+            pass
 
     def get_order_history(self, customer):
         pass
