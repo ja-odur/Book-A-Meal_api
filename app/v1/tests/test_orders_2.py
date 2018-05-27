685bfa22ea13542138d04ed3be49158ebafae1aa
Odur Joseph <odurjoseph8@gmail.com>
2018-05-27 18:30:17 +0300

    ft-Develop modified test cases for clear_order route

--- app/v1/tests/test_orders_2.py
+++ app/v1/tests/test_orders_2.py
@@ -8,10 +8,10 @@ class TestOrders2(unittest.TestCase):
         self.tester = app.test_client(self)
         self.reg_data = dict(category='caterer', email='odur@gmail.com', username='odur', password='12345',
                              confirm_password='12345', address='address1')
-        self.reg_data_user = dict(category='user', email='defaultUser@gmail.com', username='defaultUser',
+        self.reg_data_user = dict(category='user', email='defaultUser1@gmail.com', username='defaultUser1',
                                   password='12345', confirm_password='12345', address='address1')
 
-        self.login_data_user = dict(category='user', username='defaultUser', password='12345')
+        self.login_data_user = dict(category='user', username='defaultUser1', password='12345')
         self.login_data_caterer = dict(category='caterer', username='odur', password='12345')
 
         self.tester.post('api/v1/auth/signup', content_type="application/json", data=json.dumps(self.reg_data))
@@ -39,8 +39,12 @@ class TestOrders2(unittest.TestCase):
                          content_type="application/json", data=json.dumps(order1))
         self.tester.post('api/v1/orders', headers={'access-token': token_user},
                          content_type="application/json", data=json.dumps(order2))
+        resp = self.tester.get('api/v1/orders/placed', headers={'access-token': token_user})
+        order_id = json.loads(resp.data.decode())['message'][0]['order_id']
+        url_clear_order = 'api/v1/orders/clear/'+str(order_id)
 
-        self.tester.patch('api/v1/orders/clear/1', headers={'access-token':token_caterer})
+        self.tester.patch(url_clear_order, headers={'access-token':token_caterer})
+        # self.tester.patch('api/v1/orders/clear/1', headers={'access-token':token_caterer})
 
         response = self.tester.get('api/v1/orders/placed', headers={'access-token': token_user})
         response_results = json.loads(response.data.decode())
