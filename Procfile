a8d6a6ec70fa4c45e5a82bfcf9f26e99c62e79fe
odurjoseph <odurjoseph8@gmail.com>
2018-09-11 22:09:23 +0300

    fixed tests

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn app:app_heroku
+web: gunicorn heroku:app
---
+++ heroku.py
@@ -0,0 +1,21 @@
+from app.v1.views.meals import meals
+from app.v1.views.menu import menu
+from app.v1.views.users import users
+from app.v1.views.orders import orders
+
+from flasgger import Swagger
+from run_setup import app, create_app
+
+
+create_app(dev=False)
+
+swagger = Swagger(app)
+
+app.register_blueprint(users)
+
+app.register_blueprint(meals)
+
+app.register_blueprint(menu)
+
+app.register_blueprint(orders)
+
